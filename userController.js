6131d95b954dc3bcb1f2dbb8195e9a9ae9768df8
Axel Claudio Melgar <axelclaudiom@gmail.com>
2024-09-30 23:40:00 -0300

    add: module addUser

--- userController.js
+++ userController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 const { logError } = require('./utils.js');
 
 const USERS_FILE = './data/users.json';
@@ -9,6 +10,12 @@ const LOG_FILE = './error/log.json';
 function loadUsers() {
     return JSON.parse(fs.readFileSync(USERS_FILE, 'utf-8'));
 }
+
+// Guardar usuarios en el archivo
+function saveUsers(users) {
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+}
+
 
 // Mostrar todos los usuarios
 function listUsers() {
@@ -35,7 +42,34 @@ function getUserById(id) {
     }
 }
 
+// Agregar nuevo usuario
+async function addUser(nombre, apellido, email, password) {
+    try {
+        const users = loadUsers();
+        if (users.some(u => u.email === email)) {
+            throw new Error('Email ya registrado');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = {
+            id: uuidv4(),
+            nombre,
+            apellido,
+            email,
+            password: hashedPassword,
+            isLoggedIn: false
+        };
+
+        users.push(newUser);
+        saveUsers(users);
+        console.log('Usuario agregado exitosamente');
+    } catch (error) {
+        logError(error);
+    }
+}
+
 module.exports = {
     listUsers,
     getUserById,
+    addUser,
 };
